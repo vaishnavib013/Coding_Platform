67b30f85013509f22d48e60e6b72edebd38b86e3
Vaishnavi Badjate <160338989+vaishnavib013@users.noreply.github.com>
2025-02-06 21:46:02 +0530

    Add files via upload

---
+++ hackerrank/C++/c_code_using_cstudio.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+    printf("%d\n",a);
+    printf("%ld\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%lf",e);
+    return 0;
+}
