cc40383cc0f490c6c41b1f4626a949e891f760a8
Vaishnavi Badjate <160338989+vaishnavib013@users.noreply.github.com>
2025-02-24 20:35:56 +0530

    Add files via upload

---
+++ CodeCHef/python/Factorial.py
@@ -0,0 +1,15 @@
+#Factorial  Code 
+
+fact=int(input())
+i=fact-1
+while i>1:
+    fact=fact*i;
+    i=i-1
+print(fact)
+    
+
+
+"""
+Input:5
+Output:120
+"""
---
+++ CodeCHef/python/Squares_of_numbers.py
@@ -0,0 +1,22 @@
+"""
+Print Squares
+Write a program that utilizes a while loop to print the squares of numbers from 1 to 
+N
+N
+"""
+
+num = int(input())
+
+i=1
+while i<num+1:
+    print(i*i,end=" ")
+    i=i+1
+    
+    
+"""
+Sample Input
+5
+Your Output
+1 4 9 16 25 
+"""
+    
