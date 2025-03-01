c6db7de0e045838e4091ede748886bc023935ac6
Vaishnavi Badjate <160338989+vaishnavib013@users.noreply.github.com>
2025-03-01 23:56:20 +0530

    Add files via upload

---
+++ CodeCHef/c++/TIMA.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x;
+	cin>>x;
+	if ((x>1999) && (x<2051)){
+	    if(x+25>2049){
+	    cout<<"yes";
+	}
+	    else{
+	        cout<<"no";
+	        }
+	}
+	
+}
